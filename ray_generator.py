5758de1c0ee33bde6508ecd89ac34663e66289fc
Stuart Elston <selston@yelp.com>
2015-07-30 15:39:26 -0700

    prototype ray generator and sphere class

---
+++ ray_generator.py
@@ -0,0 +1,38 @@
+import math
+
+import numpy
+
+
+class RayGenerator(object):
+    # Does this actually make sense as a name?
+    # Dry this up, probably
+
+    def __init__(
+        self,
+        num_vertical_steps,
+        num_horizontal_steps,
+        horiz_fov_angle = math.pi/3.0,
+        vert_fov_angle = math.pi/3.0
+    ):
+        self.num_vertical_steps = num_vertical_steps
+        self.num_horizontal_steps = num_horizontal_steps
+
+        self.vertical_step_size = 2*math.sin(vert_fov_angle)
+        self.horizontal_step_size = 2*math.sin(horiz_fov_angle)
+
+    def yield_primary_rays(self, position, direction):
+        # TODO: actually implement this 
+        position = numpy.array([0,0,0])
+        direction = numpy.array([1,0,0])
+
+        # make sure direction is a unit vector
+        direction = direction/numpy.linalg.norm(direction)
+        horizontal_increment = numpy.array([0,1,0]) / self.horizontal_step_size
+        vertical_increment = numpy.array([0,0,1]) / self.vertical_step_size
+
+        # this won't yield unit vectors -- problem?
+        for i in xrange(self.num_vertical_steps):
+            for j in xrange(self.num_horizontal_steps):
+                horizontal_offset = horizontal_increment * (j - self.num_horizontal_steps/2)
+                vertical_offset = vertical_increment * (i - self.num_vertical_steps/2)
+                yield direction + horizontal_offset + vertical_offset
---
+++ shapes.py
@@ -0,0 +1,4 @@
+class Sphere(object):
+
+    def __init__(self, radius): #will eventually want color, too
+        self.radius = radius
