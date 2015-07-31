a3501f361ba25337252139e2c526a280975a24c7
Stuart Elston <selston@yelp.com>
2015-07-31 14:05:19 -0700

    initial sphere implementation

--- shapes.py
+++ shapes.py
@@ -1,4 +1,63 @@
-class Sphere(object):
+import numpy
 
-    def __init__(self, radius): #will eventually want color, too
+
+class Shape(object):
+
+    def find_intersection_and_normal(self, ray_pos, ray_dir):
+        """Should return a tuple of (intersection point, surface normal)"""
+        raise NotImplementedError
+
+
+class Sphere(Shape):
+
+    def __init__(self, center, radius, color):
+        self.center = center
         self.radius = radius
+        self.color = color
+
+    def find_intersection_and_normal(self, ray_pos, ray_dir):
+        """There will be an intersection if norm(ray_pos + d*ray_dir - center) = r
+        for some value of d -- i.e. if we can construct a ray that passes at
+        some point exactly r from the center of the circle.
+
+        Expanding that equation yields a quadratic in d with the components below.
+        """
+        a = numpy.linalg.norm(ray_dir)**2
+        b = 2*numpy.dot(ray_dir, ray_pos - self.center)
+        c = numpy.linalg.norm(ray_pos - self.center)**2 - self.radius**2
+
+        # import ipdb; ipdb.set_trace()
+
+        # TODO: this really should by DRYed up.  Seems like we'd want a class
+        # to handle building the intersection and the normal together and maybe
+        # testing that d >= 0
+        # Also seems like the cases where discriminant >= 0 could be combined.
+        # TODO: Apparently there's a case where we'd want the surface normal to
+        # point in the opposite direction (if ray_pos is within the sphere)
+        discriminant = b**2 - 4*a*c
+        if discriminant < 0:
+            return None, None
+        elif discriminant == 0:
+            d = -b/a
+            if d < 0:
+                return None, None
+            else:
+                intersection = ray_pos + d*ray_dir
+                return intersection, self.build_surface_normal(intersection)
+        else:
+            d1 = (-b + discriminant)/(2*a)
+            d2 = (-b - discriminant)/(2*a)
+            best_d = None
+            for potential_d in (d1, d2):
+                if potential_d < 0:
+                    continue
+                else:
+                    # best_d = min(best_d or 0, potential_d)
+                    best_d = potential_d if best_d is None else min(best_d, potential_d)
+            if best_d is None:
+                return None, None
+            intersection = ray_pos + best_d*ray_dir
+            return intersection, self.build_surface_normal(intersection)
+
+    def build_surface_normal(self, intersection):
+        return intersection - self.center
