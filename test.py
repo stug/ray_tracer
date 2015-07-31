0f1c257f92919c727bd12b03515f3bb30896b32f
Stuart Elston <selston@yelp.com>
2015-07-31 14:06:14 -0700

    test script

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-from ray_tracer import RayTracer
+import numpy
 
-tracer = RayTracer(10, 10)
+import colors
+from ray_tracer import RayTracer
+from scene import Scene
+from shapes import Sphere
+
+scene = Scene(
+    position=numpy.array([0,0,0]),
+    direction=numpy.array([1,0,0]),  # this is the only direction currently supported :-/
+    shapes=[
+        Sphere(
+            center=numpy.array([9,-1,0]),
+            radius=2,
+            color=colors.BLUE
+        ),
+        Sphere(
+            center=numpy.array([10, 1, 0]),
+            radius=3,
+            color=colors.GREEN
+        )
+    ]
+)
+
+tracer = RayTracer(scene, 200, 200)
 tracer.trace_scene()
 
 print tracer.screen
