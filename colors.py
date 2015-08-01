baf279a135532b2b1882d3cb751601a08223f1f9
Stuart Elston <selston@yelp.com>
2015-07-31 17:29:38 -0700

    added grey color

--- colors.py
+++ colors.py
@@ -6,6 +6,7 @@ import numpy
 
 BLACK = numpy.array([0,0,0])
 WHITE = numpy.array([255,255,255])
+GREY_50 = numpy.array([127,127,127])
 RED = numpy.array([255,0,0])
 GREEN = numpy.array([0,255,0])
 BLUE = numpy.array([0,0,255])
