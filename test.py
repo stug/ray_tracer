811a46b40da3f33932676b95ab6bda75a5f3e135
Stu Elston <stuart.b.elston@gmail.com>
2015-11-13 14:32:07 -0800

    more complicated test scene

--- test.py
+++ test.py
@@ -7,26 +7,48 @@ from scene import Scene
 from shapes import Sphere
 
 scene = Scene(
-    position=numpy.array([0,0,0]),
+    position=numpy.array([-5,0,0]),
     direction=numpy.array([1,0,0]),  # this is the only direction currently supported :-/
-    background_color=colors.GREY_50,
+    background_color=colors.BLACK,
     shapes=[
         Sphere(
             center=numpy.array([9,-1,0]),
             radius=2,
-            color=colors.BLUE
+            color=colors.BLUE,
+            specular=1
         ),
         Sphere(
-            center=numpy.array([10, 1, 0]),
+            center=numpy.array([10, 2, 0]),
             radius=3,
-            color=colors.RED
+            color=colors.RED,
+            specular=1
+        ),
+        Sphere(
+            center=numpy.array([5,0,3]),
+            radius=0.5,
+            color=colors.GREEN,
+            specular=1
+        ),
+        Sphere(
+            center=numpy.array([5,-4,0]),
+            radius=0.5,
+            color=colors.MAGENTA,
+            specular=1
+        ),
+        Sphere(
+            center=numpy.array([8,0,-10]),
+            radius=7,
+            color=colors.YELLOW,
+            specular=1
         )
     ],
-    light_sources=[LightSource(numpy.array([9,-7,3]))]
+    light_sources=[
+        LightSource(numpy.array([0,0,0]))
+    ]
 )
 
 
 if __name__ == '__main__':
-    tracer = RayTracer(scene, 200, 200)
+    tracer = RayTracer(scene, 900, 900)
     tracer.trace_scene()
     tracer.dump_scene_to_png('test.png')
