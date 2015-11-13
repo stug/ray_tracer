33bd3bbc80e8c7b6c890424c60611516255919a8
Stu Elston <stuart.b.elston@gmail.com>
2015-11-13 14:31:04 -0800

    add CMY colors

--- colors.py
+++ colors.py
@@ -7,6 +7,11 @@ import numpy
 BLACK = numpy.array([0,0,0])
 WHITE = numpy.array([255,255,255])
 GREY_50 = numpy.array([127,127,127])
+
 RED = numpy.array([255,0,0])
 GREEN = numpy.array([0,255,0])
 BLUE = numpy.array([0,0,255])
+
+CYAN = numpy.array([0,255,255])
+MAGENTA = numpy.array([255,0,255])
+YELLOW = numpy.array([255,255,0])
