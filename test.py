bf9214202ed2ca518cdb7de9fe665fafe213f644
Stu Elston <stuart.b.elston@gmail.com>
2015-11-13 19:23:52 -0800

    added a new test scene

--- test.py
+++ test.py
@@ -5,50 +5,86 @@ from ray_tracer import RayTracer
 from scene import LightSource
 from scene import Scene
 from shapes import Sphere
+from shapes import ZPlane
 
-scene = Scene(
-    position=numpy.array([-5,0,0]),
-    direction=numpy.array([1,0,0]),  # this is the only direction currently supported :-/
+scene1 = Scene(
+    position=numpy.array([-13,0,0]),
+    direction=numpy.array([1,0,0]),
     background_color=colors.BLACK,
     shapes=[
         Sphere(
             center=numpy.array([9,-1,0]),
             radius=2,
             color=colors.BLUE,
-            specular=1
+            specular=0.8
         ),
         Sphere(
             center=numpy.array([10, 2, 0]),
             radius=3,
             color=colors.RED,
-            specular=1
+            specular=0.8
         ),
         Sphere(
             center=numpy.array([5,0,3]),
             radius=0.5,
             color=colors.GREEN,
-            specular=1
+            specular=0.8
         ),
         Sphere(
             center=numpy.array([5,-4,0]),
             radius=0.5,
             color=colors.MAGENTA,
-            specular=1
+            specular=0.8
         ),
         Sphere(
             center=numpy.array([8,0,-10]),
             radius=7,
             color=colors.YELLOW,
-            specular=1
+            specular=0.8
+        ),
+        ZPlane(
+            z_coord=-4,
+            color=colors.WHITE,
+            checkered=True,
+            specular=0.8
         )
     ],
     light_sources=[
-        LightSource(numpy.array([0,0,0]))
+        LightSource(numpy.array([0,0,0])),
+    ]
+)
+
+scene2 = Scene(
+    position=numpy.array([0,0,0]),
+    direction=numpy.array([1,0,0]),
+    background_color=colors.BLACK,
+    shapes=[
+        Sphere(
+            center=numpy.array([0,0,0]),
+            radius=10,
+            color=colors.GREY_50,
+            specular=0.9
+        ),
+        Sphere(
+            center=numpy.array([4,1,1]),
+            radius=0.5,
+            color=colors.RED,
+            specular=0.5
+        ),
+        ZPlane(
+            z_coord=-2,
+            color=colors.WHITE,
+            checkered=True,
+            specular=0
+        )
+    ],
+    light_sources=[
+        LightSource(numpy.array([0,-7,0])),
     ]
 )
 
 
 if __name__ == '__main__':
-    tracer = RayTracer(scene, 900, 900)
+    tracer = RayTracer(scene2, 300, 300)
     tracer.trace_scene()
     tracer.dump_scene_to_png('test.png')
